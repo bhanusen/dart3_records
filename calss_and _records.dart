d10b71349a62cfef1fd37ffd7a6bee2a1599930b
bhanusen <virtualinfo68343@gmail.com>
2025-10-19 10:54:04 +0530

    Update calss_and _records.dart

--- calss_and _records.dart
+++ calss_and _records.dart
@@ -1,1 +1,24 @@
+void main() async {
+   
+ 
+ final p = Cat('K');
+  final Animal( :name,:color,:m  )  = Animal('B','C',Cat('K'));
+ 
+  print(name);
+  print(m.meu); 
+ 
+}
+class Animal {
+  
+  String name;
+  String color;
+  Cat m;
+  Animal(this.name,this.color, this.m);
+}
 
+class  Cat{
+  
+  String meu;
+  
+  Cat(this.meu);
+}
